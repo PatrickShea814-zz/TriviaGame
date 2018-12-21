d1ad00032886a029b7fe36d2d7cceaadf6a89140
Pat Shea <44069256+PatrickShea814@users.noreply.github.com>
2018-12-21 13:15:20 -0500

    New GIFs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 
 
+
 $(document).mouseover(function () {
     document.getElementById("my_audio").play();
 });
@@ -13,9 +14,9 @@ $(document).ready(function () {
             photo: "assets/images/pillar.jpg"
         },
         {
-            question: "What is 343 Guilty Spark?",
+            question: "Who is the Monitor of Halo Installation 05?",
             choice: ["2401 Penitent Tangent", "343 Guilty Spark", "Mendicant Bias", "867 Guilty Tangent"],
-            answer: 1,
+            answer: 0,
             photo: "assets/images/monitor.png"
         },
         {
@@ -38,7 +39,7 @@ $(document).ready(function () {
         },
         {
             question: "What was Foehammer's call-sign?",
-            choice: ["John 177", "Charlie 258", "Echo 419", "Whisky 327"],
+            choice: ["John 117", "Charlie 258", "Echo 419", "Whisky 327"],
             answer: 2,
             photo: "assets/images/Echo_419_and_Bravo_022.jpg"
         },
@@ -69,7 +70,7 @@ $(document).ready(function () {
     var wrongCount = 0;
     var killdeathRatio = correctCount / wrongCount;
     var unanswerCount = 0;
-    var timer = 59;
+    var timer = 29;
     var intervalId;
     var userGuess = "";
     var running = false;
@@ -80,9 +81,11 @@ $(document).ready(function () {
     var holder = [];
 
     $("#reset").hide();
+    $("#success").hide();
+    $("#failed").hide();
     //click mission start to begin
     $("#start").on("click", function () {
-        $("#timeleft").html("<h2>" + "Time remaining: 60 Seconds" + "</h2>");
+        $("#timeleft").html("<h2>" + "Time remaining: 30 Seconds" + "</h2>");
         $("#start").hide();
         $("#instruc").hide();
         runTimer();
@@ -106,19 +109,31 @@ $(document).ready(function () {
 
         //stop timer if it reaches 0
         if (timer === -1) {
-            $("#timeleft").html("<h2>" + "MISSION END:" + "</h2>");
-            unanswerCount++;
-            stop();
-            $("#questionblock").empty();
-            $("#answerblock").empty();
-            $("#questionblock").html("<h2>CARNAGE REPORT: </h2>");
-            $("#answerblock").append("<h2> Kills: " + correctCount + "</h2>");
-            $("#answerblock").append("<h2> Deaths: " + wrongCount + "</h2>");
-            $("#answerblock").append("<h2> K/D Ratio: " + killdeathRatio + "</h2>");
-            $("#answerblock").append("<h2> Unanswered: " + unanswerCount + "</h2>");
-            $("#reset").show();
-
-
+            if (correctCount > 7) {
+                $("#timeleft").html("<h2>" + "MISSION SUCCESS:" + "</h2>");
+                unanswerCount++;
+                stop();
+                $("#questionblock").empty();
+                $("#answerblock").empty();
+                $("#questionblock").html("<h2>CARNAGE REPORT: </h2>");
+                $("#answerblock").append("<h2> Kills: " + correctCount + "</h2>");
+                $("#answerblock").append("<h2> Deaths: " + wrongCount + "</h2>");
+                $("#answerblock").append("<h2> K/D Ratio: " + killdeathRatio + "</h2>");
+                $("#success").show();
+                $("#reset").show();
+            } else {
+                $("#timeleft").html("<h2>" + "MISSION FAILED:" + "</h2>");
+                unanswerCount++;
+                stop();
+                $("#questionblock").empty();
+                $("#answerblock").empty();
+                $("#questionblock").html("<h2>CARNAGE REPORT: </h2>");
+                $("#answerblock").append("<h2> Kills: " + correctCount + "</h2>");
+                $("#answerblock").append("<h2> Deaths: " + wrongCount + "</h2>");
+                $("#answerblock").append("<h2> K/D Ratio: " + killdeathRatio + "</h2>");
+                $("#failed").show();
+                $("#reset").show();
+            }
         }
     }
 
@@ -180,15 +195,31 @@ $(document).ready(function () {
 
             //run the score screen if all questions answered
             if ((wrongCount + correctCount + unanswerCount) === qCount) {
-                $("#questionblock").empty();
-                $("#questionblock").html("<h2>Carnage Report: </h2>");
-                $("#answerblock").append("<h2> Correct: " + correctCount + "</h2>");
-                $("#answerblock").append("<h2> Incorrect: " + wrongCount + "</h2>");
-                $("#answerblock").append("<h2> K/D Ratio: " + killdeathRatio + "</h2>");
-                $("#answerblock").append("<h2> Unanswered: " + unanswerCount + "</h2>");
-                $("#reset").show();
-
-
+                if (correctCount > 7) {
+                    $("#timeleft").html("<h2>" + "MISSION SUCCESS:" + "</h2>");
+                    unanswerCount++;
+                    stop();
+                    $("#questionblock").empty();
+                    $("#answerblock").empty();
+                    $("#questionblock").html("<h2>CARNAGE REPORT: </h2>");
+                    $("#answerblock").append("<h2> Kills: " + correctCount + "</h2>");
+                    $("#answerblock").append("<h2> Deaths: " + wrongCount + "</h2>");
+                    $("#answerblock").append("<h2> K/D Ratio: " + killdeathRatio + "</h2>");
+                    $("#success").show();
+                    $("#reset").show();
+                } else {
+                    $("#timeleft").html("<h2>" + "MISSION FAILED:" + "</h2>");
+                    unanswerCount++;
+                    stop();
+                    $("#questionblock").empty();
+                    $("#answerblock").empty();
+                    $("#questionblock").html("<h2>CARNAGE REPORT: </h2>");
+                    $("#answerblock").append("<h2> Kills: " + correctCount + "</h2>");
+                    $("#answerblock").append("<h2> Deaths: " + wrongCount + "</h2>");
+                    $("#answerblock").append("<h2> K/D Ratio: " + killdeathRatio + "</h2>");
+                    $("#failed").show();
+                    $("#reset").show();
+                }
             } else {
                 runTimer();
                 displayQuestion();
@@ -197,14 +228,15 @@ $(document).ready(function () {
     }
 
     $("#reset").on("click", function () {
+        $("#success").hide();
+        $("#failed").hide();
         $("#reset").hide();
-        timer = 20;
+        timer = 29;
         $("#answerblock").empty();
         $("#questionblock").empty();
         for (var i = 0; i < holder.length; i++) {
             options.push(holder[i]);
         }
-        // hidepicture();
         runTimer();
         displayQuestion();
 
